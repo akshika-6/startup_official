f7690b4baf6f82f7559afb72bc78c1fd7404080b
Akshika Gupta <akshika27ak@gmail.com>
2025-07-21 18:36:23 +0530

    e

--- routes/dashboardRoutes.mjs
+++ routes/dashboardRoutes.mjs
@@ -1,11 +1,11 @@
-// routes/dashboardRoutes.mjs
 import express from 'express';
 import { getDashboard } from '../controllers/dashboardController.mjs';
-import { authorizeRoles } from '../middleware/auth.mjs';
+import { protect } from '../middleware/auth.mjs';
 
 const router = express.Router();
 
 // GET /api/dashboard
-router.get('/', authenticateUser, getDashboard);
+router.get('/', protect, getDashboard);
 
 export default router;
+
